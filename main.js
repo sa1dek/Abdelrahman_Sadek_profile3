d1587ab54ff9c4f9894a128be9cebd56f00db190
sa1dek <abdelrahman20e@gmail.com>
2025-08-22 12:28:31 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -57,26 +57,26 @@ const sr = ScrollReveal({
     reset: true
 })
 
-sr.reveal('.home-title', {});
-sr.reveal('.home-scroll', { delay: 200 });
-sr.reveal('.home-img', { origin: 'right', delay: 400 });
+// sr.reveal('.home-title', {});
+// sr.reveal('.home-scroll', { delay: 200 });
+// sr.reveal('.home-img', { origin: 'right', delay: 400 });
 
-sr.reveal('.about-img', { delay: 500 });
-sr.reveal('.about-subtitle', { delay: 300 });
-sr.reveal('.about-profession', { delay: 400 });
-sr.reveal('.about-text', { delay: 500 });
-sr.reveal('.about-social-icon', { delay: 600, interval: 200 });
+// sr.reveal('.about-img', { delay: 500 });
+// sr.reveal('.about-subtitle', { delay: 300 });
+// sr.reveal('.about-profession', { delay: 400 });
+// sr.reveal('.about-text', { delay: 500 });
+// sr.reveal('.about-social-icon', { delay: 600, interval: 200 });
 
-sr.reveal('.skills-subtitle', {});
-sr.reveal('.skills-name', { distance: '20px', delay: 50, interval: 100 });
-sr.reveal('.skills-img', { delay: 400 });
+// sr.reveal('.skills-subtitle', {});
+// sr.reveal('.skills-name', { distance: '20px', delay: 50, interval: 100 });
+// sr.reveal('.skills-img', { delay: 400 });
 
-sr.reveal('.portfolio-img', { interval: 200 });
+// sr.reveal('.portfolio-img', { interval: 200 });
 
-sr.reveal('.contact-subtitle', {});
-sr.reveal('.contact-text', { interval: 200 });
-sr.reveal('.contact-input', { delay: 400 });
-sr.reveal('.contact-button', { delay: 600 });
+// sr.reveal('.contact-subtitle', {});
+// sr.reveal('.contact-text', { interval: 200 });
+// sr.reveal('.contact-input', { delay: 400 });
+// sr.reveal('.contact-button', { delay: 600 });
 
 
 
